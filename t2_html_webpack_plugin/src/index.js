a280715c10795cb4991b5876dd76c8a78620e255
Quinton <bolt.quinton@yahoo.com>
2017-11-24 23:54:06 -0600

    created example of using html-webpack-plugin and extract-text-webpack-plugin for bundling css and generating a html file based on dependencies

--- t2_html_webpack_plugin/src/index.js
+++ t2_html_webpack_plugin/src/index.js
@@ -1,2 +1,4 @@
 import { greet } from './util';
+import './myStyles.css';
+
 greet();
---
+++ t2_html_webpack_plugin/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/build',
+        filename: './build/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['babel-preset-env']
+                }
+              }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new ExtractTextPlugin("styles.css")
+    ]
+};
